93fe8b25b829b14660d8cf9d6d090eeff30e1aae
Leho <leho@leho.dev>
2022-05-10 21:39:01 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Made By Leho | leho.dev | github.com/lehoooo
+import time
+import discum
+from discum.utils.slash import SlashCommander
+
+# MAKE SURE TO DM THE BOT ANYTHING BEFORE RUNNING FOR THE FIRST TIME
+
+botid = "302050872383242240" #current bot id for disboard change if needed
+channelid = "CHANNEL ID HERE" #current channel id to send to
+guildid = "GUILDID HERE" #current guild id to send to
+
+
+bot = discum.Client(token="YOUR TOKEN HERE :)", log=False)
+
+while True:
+    print("Bumping...")
+    slashCmds = bot.getSlashCommands(str(botid)).json()
+    s = SlashCommander(slashCmds)
+    a = s.get(["bump"])
+    bot.triggerSlashCommand(str(botid), str(channelid), guildID=str(guildid), data=a)
+    print("Bumped!")
+    print("Waiting...")
+    time.sleep(120 * 60)
